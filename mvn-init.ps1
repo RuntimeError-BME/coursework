e50a22c0fc9d45334beb8a3d4c5d994e91e32b45
Kristóf Váradi <vkristof20@protonmail.com>
2023-04-17 09:48:31 +0200

    extended build script

---
+++ mvn-init.ps1
@@ -0,0 +1,23 @@
+# Set the URL and file name of the Apache Maven binary distribution archive
+$url = "https://www.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz"
+$fileName = "apache-maven-3.6.3-bin.tar.gz"
+
+# Set the directory to extract the archive to
+$extractDir = "./dist/maven"
+
+# Download the archive
+Invoke-WebRequest -Uri $url -OutFile $fileName
+
+# Extract the archive
+New-Item -ItemType Directory -Force -Path $extractDir
+tar -xzf $fileName -C $extractDir --strip-components=1
+
+# Add the Apache Maven "bin" directory to the PATH environment variable
+$env:Path += ";$extractDir\bin"
+
+mvn clean install
+mvn package
+
+cd ./target
+
+java -cp .\target\coursework-1.0-SNAPSHOT.jar org.runtimeerror.Main
