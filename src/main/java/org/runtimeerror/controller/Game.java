35cdd418714be1a57da8d9740602aa4af06b7751
Ádám Mizser <mizser.adam@gmail.com>
2023-05-01 16:05:04 +0200

    alpha 0.0.1

--- src/main/java/org/runtimeerror/controller/Game.java
+++ src/main/java/org/runtimeerror/controller/Game.java
@@ -1,73 +1,84 @@
 package org.runtimeerror.controller;
+
 import org.runtimeerror.Main;
 import org.runtimeerror.model.map.*;
 import org.runtimeerror.model.players.Technician;
 import org.runtimeerror.model.players.Player;
-import static org.runtimeerror.skeleton.SkeletonController._Game;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Random;
 
 /**
  * A játékmenetet kezeli (vizsgálja, hogy véget ért-e a játék, valamint a köröket vezérli).
- * Számontartja és módosítja a csőrendszert (turn-önként véletlenszerűen pumpákat ront el, round-onként pedig ciszternák szomszédos üres helyeire új csöveket helyez le).
- * Turn-ök elején folyatja a vizet a forrásokból a szabad végű / lyukas csövekig vagy ciszternáig, illetve pontokat tud osztani a csapatoknak, amit szintén számontart.
+ * Számontartja és módosítja a csőrendszert (turn-önként véletlenszerűen pumpákat ront el,
+ * round-onként pedig ciszternák szomszédos üres helyeire új csöveket helyez le).
+ * Turn-ök elején folyatja a vizet a forrásokból a szabad végű / lyukas csövekig vagy ciszternáig,
+ * illetve pontokat tud osztani a csapatoknak, amit szintén számontart.
  */
 public class Game {
     /**
      * Attribútumok
      */
-    private int currPlayerIdx = 0; // a soron lévő játékos indexe a „players” gyűjteményben
-    private int scoreTechnician = 0; // a szerelők pontszáma
-    private int scoreSaboteur = 0; // a szabotőrök pontszáma
+    private int currPlayerIdx; // a soron lévő játékos indexe a „players” gyűjteményben
+    private int scoreTechnician; // a szerelők pontszáma
+    private int scoreSaboteur; // a szabotőrök pontszáma
     private List<Player> players; // a játékban résztvevő játékosok
     private Network network; // a pálya elemeit tárolja, szortírozza
 
+    private static Game singleInstance; // a játék egyetlen példánya (Singleton)
+    private Random random; // véletlenszerű viselkedés lesz megvalósítva vele
+
     /**
-     * Konstruktor
+     * Konstruktor (privát láthatóságú, mert Singleton osztály).
+     * A Reset függvényt hívja, ami alapállapotba helyezi a játékot.
      */
-    public Game(){
-        currPlayerIdx = 0; // a soron lévő játékos indexe a „players” gyűjteményben
+    private Game() {
+        Reset();
+    }
+
+    /**
+     * Visszaadja a játék egyetlen példányát. (Első híváskor hozza létre.)
+     */
+    public static Game GetInstance() {
+        if (singleInstance == null)
+            singleInstance = new Game();
+
+        return singleInstance;
+    }
+
+    /**
+     * Alapállapotba (egyetlen forrás, nem lesznek játékosok és kiosztott pontok, stb.) állítja az egész játékot.
+     */
+    public void Reset() {
+        currPlayerIdx   = 0; // a soron lévő játékos indexe a „players” gyűjteményben
         scoreTechnician = 0; // a szerelők pontszáma
-        scoreSaboteur = 0; // a szabotőrök pontszáma
-        players=new ArrayList<>(); // a játékban résztvevő játékosok
-        network=null; // a pálya elemeit tárolja, szortírozza
-    }
-
-    /**
-     * Metódusok
-     */
-
-    /** DEBUGHOZ */
-    public void setCurrPlayerIdx(int idx){
-        currPlayerIdx = idx;
-    }
-
-    public void setNetwork(Network network) {
-        this.network = network;
+        scoreSaboteur   = 0; // a szabotőrök pontszáma
+        players         = new ArrayList<>(6); // a játékban résztvevő játékosok
+        network         = new Network(); // a pálya elemeit tárolja, szortírozza
+        network.AddSource(new Source()); // csak egyetlen forrást tartalmaz kezdetben a pálya
+        singleInstance = null; // a játék egyetlen példánya (Singleton)
+        random         = new Random(); // véletlenszerű viselkedés lesz megvalósítva vele
     }
 
     /** Átadott számú pontot ad a szerelőknek. */
     public void AddTechnicianPoints(int points) {
-        Main.skeleton.PrintFunctionCalled(this);
         scoreTechnician += points;
-        Main.skeleton.PrintFunctionReturned("AddTechnicianPoints","");
     }
 
     /** Átadott számú pontot ad a szabotőröknek. */
     public void AddSaboteurPoints(int points) {
-        Main.skeleton.PrintFunctionCalled(this);
         scoreSaboteur += points;
-        Main.skeleton.PrintFunctionReturned("AddSaboteurPoints","");
     }
 
-    /** A következő turn indítása (áramoltatja a vizet, pontokat oszt, véletlenszerűen pumpákat ront el,
-     és ha véget ért egy teljes kör (round), akkor új csöveket teremt a ciszternák üres szomszédjai helyén).
+    /** A következő játékos turn-jét indítja el (véletlenszerűen pumpákat ront el, csökkenti a ragadós/csúszós elemek
+     időszámlálóját alaphelyzetbe állítva őket a lejártával, áramoltatja a vizet, pontokat oszt a csapatoknak,
+     és ha véget ért egy teljes kör (round), akkor új egy-egy új csövet teremt a ciszternák üres szomszédjai helyén).
      Ha az egyik csapat elérte a győzelemhez szükséges pontok számát, véget vet a játéknak. */
     public void NextTurn() {
-        Main.skeleton.PrintFunctionCalled(this);
-        //Next turn -> Source-okra flood, és csövek countereit csökkenti amelyek nagyobbak 0nál vagy ha 0 akkor a sticky-t és slippery-t is falsera állítja!
-        Main.skeleton.PrintFunctionReturned("NextTurn","");
+        for (Pump pump : network.GetPumps()) {
+
+        }
+
     }
 
     /** Visszaadja a játékost, aki éppen soron van (akinek turnje van jelenleg). */
@@ -95,6 +106,14 @@ public class Game {
     /** Inner-class. A felhasználót és a játékot köti össze. Irányokat / célpontokat tud bekérni a felhasználótól.
      Játékosmozgatást, cső- és pumpalehelyezést, illetve csőfelvevést kezdeményezhet vele. */
     public static class Input {
+
+        /**
+         *
+         */
+        public void EndGame() {
+
+            System.out.println("");
+        }
 
         /** d irányba próbálja mozgatni az épp soron lévő játékost, arról az elemről, amelyiken éppen tartózkodik. */
 //        public void MoveCurrPlayer(Direction d) {
@@ -187,6 +206,15 @@ public class Game {
             Main.skeleton.PrintFunctionReturned("GetNewPumpDirections", "dirs");
             return dirs;
         }
+    }
+
+
+    /**
+     * Megadja, hogy egy tetszőleges cső el legyen-e törve (logikai értéked ad vissza).
+     */
+    private boolean shouldBreakPipe(boolean deterministic) {
+        if (deterministic)
+            return false;
     }
 
     /** --------- CSAK A JÁTÉK INICIALIZÁLÁSKOR HASZNÁLT FÜGGVÉNYEK (nincs hibakezelés bennük): --------- */
--- src/main/java/org/runtimeerror/prototype/PrototypeController.java
+++ src/main/java/org/runtimeerror/prototype/PrototypeController.java
@@ -1,6 +1,23 @@
 package org.runtimeerror.prototype;
 
+import org.runtimeerror.controller.Game;
+
+import java.io.InputStream;
+import java.io.PrintStream;
 public class PrototypeController {
+
+    private Game game = Game.GetInstance(); // egy referencia a játék egyetlen példányára (Singleton)
+
+    private static PrintStream outConsole = System.out; // referencia a konzol kimenetre
+    private static InputStream inConsole = System.in; // referencia a konzol bemenetre
+
+    private static boolean isRandom = true; // megadja, hogy a modell véletlenszerű viselkedése be van-e kapcsolva
+
+
+    public static boolean GetRandom
+
+    public static void ToggleRandom
+
         /**
          * Ide kell a bemenet olvasás annak végrehajtása és a kimenet írása
          */
