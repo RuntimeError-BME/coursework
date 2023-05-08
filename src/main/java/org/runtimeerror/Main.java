5cbdf86359cc9b593146fa15311c44df92b740c5
sasy <szabolcs.attila.sasvari@gmail.com>
2023-05-08 13:10:14 +0200

    Added modification ideas as comment (Main.java)

--- src/main/java/org/runtimeerror/Main.java
+++ src/main/java/org/runtimeerror/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
 
 /* Nagyobb módosítások a projektben:
-
-
+    Gráfos megoldás, index kezelés a teljes pályársa - kezdetben incializálás, utána indexek frissítése az aktuális változásokat követően (index nem változik ha felveszi a pályáról, és leteszi utána).
+    GUI-ban grid helyett pozíció generátor függvény, illetve button forgatás megfelelő lehet.
+    Játékos egérrel tudja irányítani a karaktert a pálya elemein, amely indexet küld a játéknak.
  */
