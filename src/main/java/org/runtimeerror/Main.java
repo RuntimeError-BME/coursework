8d642b56dbfe2bf7c97fd35d3af469e9cb880f3b
sasy <szabolcs.attila.sasvari@gmail.com>
2023-05-21 14:54:34 +0200

    Update

--- src/main/java/org/runtimeerror/Main.java
+++ src/main/java/org/runtimeerror/Main.java
@@ -12,6 +12,6 @@ public class Main {
         // pc.ResetState();
 
         /** GUI */
-        GuiController guic = new GuiController();
+        GuiController guic = GuiController.GetInstance();
     }
 }
